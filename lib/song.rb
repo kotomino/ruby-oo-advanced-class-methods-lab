4dcc819325dbeb015bfb24b53cfcbfda61ee2a47
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2020-11-19 15:42:35 -0500

    commit tree d7a6d8cbdd4060ea49fa3b1f8c3503705b9f5772

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,56 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if exists = self.find_by_name(name)
+      exists
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    song = self.new
+    delimiters = [".", " - "]
+    song.name = filename.split(Regexp.union(delimiters))[1]
+    song.artist_name = filename.split(" - ")[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    self.all << self.new_from_filename(filename)
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
+
+# "Thundercat - For Love I Come.mp3"
